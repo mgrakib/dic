a5ac1ab11c344e2e2f083a9f2a58e2e8608598de
mgrakib <mgrakibbd@gmail.com>
2023-02-27 19:38:40 +0600

    update

--- js/dictionary_six.js
+++ js/dictionary_six.js
@@ -1,54 +1,165 @@
-const inputValue = document.getElementById("input-value");
-const word = document.getElementById("word");
-const phonetic = document.getElementById("phonetic");
+// get element 
+const getElement = (id) => document.getElementById(id);
 
-// container 
-const container = document.getElementById("container");
 
+// elements
+const inputFeild = getElement("input-feild");
+const errowShow = getElement("errow-show");
+const errowMessage = getElement("errow-message");
+const wordName = getElement("word-name");
+const wordPhonetic = getElement("word-phonetic");
+const audioContainer = getElement("audio-container");
+const meaningContainer = getElement("meaning-container");
+
+
+
+// get input value 
 const getData = () => {
-    const value = inputValue.value;
-    console.log(value);
-    loadData(value);
-    inputValue.value = '';
+    const inputValue = inputFeild.value;
+    loadData(inputValue);
 }
-
 
 // load data 
-const loadData = async (word) => {
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+const loadData = async (value) => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
     const res = await fetch(url);
     const data = await res.json();
-    setData(data);
+
+    if (data.title === "No Definitions Found") {
+        errowShow.classList.remove('hidden');
+        errowMessage.innerText = data.message;
+        setTimeout(function () {
+            errowShow.classList.add("hidden");
+        }, 3000)
+    } else {
+        setData(data[0]);
+    }
 }
 
 
-const setData = data => { 
-    const info = data[0];
-    word.innerText = info.word;
-    phonetic.innerText = info.phonetic;
-    container.innerHTML = '';
+const setData = (info) => {
+    // to see value 
+    console.log(info);
+    inputFeild.value = "";
+    // object destracturtring
+    const { word, phonetic, phonetics, meanings } = info;
 
-    const meaning = info.meanings;
-    meaning.forEach(element => {
-        const div = document.createElement('div');
-        div.classList = 'py-4';
-        const p = document.createElement('p');
-        p.innerText = element.partOfSpeech;
-        const ul = document.createElement('ul');
+    // set value 
+    wordName.innerText = word;
+    wordPhonetic.innerText = phonetic;
+   
 
-        element.definitions.forEach(rakib => {
-            const li = document.createElement('li');
-            li.classList = "list-disc";
-			li.innerText = rakib.definition;
+
+    phonetics.forEach(element => {
+        // console.log(element.audio);
+        if (element.audio) {
+            audioContainer.innerHTML = '';
+            const audio = document.createElement("audio");
+            audio.src = element.audio;
+
+            const div = document.createElement("div");
+            div.innerHTML = `<div onclick="playAudio(${audio})"  id="playAudio" class="w-[40px] md:w-[60px] h-[40px] md:h-[60px] bg-gray-400 rounded-full flex items-center justify-center">
+            <img class="w-[30%]" src="./img/play.png.crdownload" alt="">        
+              </div>`;
+            div.appendChild(audio);
+            div.onclick = () => {
+                audio.play();
+            }
+
+            audioContainer.appendChild(div);
+
+        }
+    })
+
+
+
+    // definations 
+    meaningContainer.innerHTML = "";
+    meanings.forEach(meaning => {
+		// main div
+		const mainDiv = document.createElement("div");
+		// h1
+		const h1 = document.createElement("h1");
+		h1.classList.add("text-success", "font-bold", "text-3xl");
+		h1.innerText = meaning.partOfSpeech;
+
+
+        // seconddiv ***************
+		// ol container
+		const div = document.createElement("div");
+		div.classList.add("py-4");
+
+		// h4
+		const h4 = document.createElement("h4");
+		h4.classList.add("text-accent", "text-2xl");
+		h4.innerText = "Meaning";
+		div.appendChild(h4);
+
+		// ul
+		const ul = document.createElement("ul");
+		ul.classList.add("py-3", "ml-5", "list-decimal");
+		ul.innerHTML = "";
+
+		// defination
+		meaning.definitions.forEach(def => {
+			const li = document.createElement("li");
+			li.innerText = def.definition;
 			ul.appendChild(li);
-			
 		});
 
-        div.appendChild(p);
-        div.appendChild(ul);
-        container.appendChild(div);
-    });
+		div.appendChild(ul);
 
-    
+        console.log(meaning.synonyms.length);
+        if (meaning.synonyms.length !== 0) {
+            const synonymsContainer = document.createElement("div");
+			synonymsContainer.innerHTML = "";
+			const synonymsTitle = document.createElement("span");
+			synonymsTitle.innerHTML = `<span class="text-2xl text-gray-500 mr-2">Synonyms:</span> `;
+			synonymsContainer.appendChild(synonymsTitle);
+
+			meaning.synonyms.forEach(syn => {
+				const synonymsDiv = document.createElement("span");
+				synonymsDiv.innerText = syn + " , ";
+				synonymsContainer.appendChild(synonymsDiv);
+			});
+
+			div.appendChild(synonymsContainer);
+        } else {
+            
+        }
+
+        
+		
+
+
+		mainDiv.appendChild(h1);
+		mainDiv.appendChild(div);
+
+		meaningContainer.appendChild(mainDiv);
+	})
+
+
+    // synonims 
+
+
 }
-    
+
+
+loadData('keyboard');
+
+
+const playAudio = (audio) => {
+    console.log(audio);
+}
+
+
+
+
+
+
+
+
+
+
+
+       
